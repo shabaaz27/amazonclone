48f507c93ba930814c1890ce2355eecfad756a08
shabaaz27 <khanshabaaz05@gmail.com>
2022-03-26 16:15:46 +0530

    page header design for web done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Header/>
+      {/* Home */}
+
     </div>
   );
 }
